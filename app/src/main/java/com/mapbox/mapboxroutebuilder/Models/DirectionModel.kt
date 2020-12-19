726975ae3e695adb833c7b96bc1d7ec46f745493
Rob <robgas45@gmail.com>
2020-12-20 00:23:05 +0400

    Direction Model and some changes.

---
+++ app/src/main/java/com/mapbox/mapboxroutebuilder/Models/DirectionModel.kt
@@ -0,0 +1,61 @@
+package com.mapbox.mapboxroutebuilder.Models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DirectionModel(
+    @SerializedName("code")
+    val code: String,
+    @SerializedName("routes")
+    val routes: List<Route>,
+    @SerializedName("uuid")
+    val uuid: String,
+    @SerializedName("waypoints")
+    val waypoints: List<Waypoint>
+) {
+    data class Route(
+        @SerializedName("distance")
+        val distance: Double,
+        @SerializedName("duration")
+        val duration: Double,
+        @SerializedName("geometry")
+        val geometry: String,
+        @SerializedName("legs")
+        val legs: List<Leg>,
+        @SerializedName("weight")
+        val weight: Double,
+        @SerializedName("weight_name")
+        val weightName: String
+    ) {
+        data class Leg(
+            @SerializedName("admins")
+            val admins: List<Admin>,
+            @SerializedName("distance")
+            val distance: Double,
+            @SerializedName("duration")
+            val duration: Double,
+            @SerializedName("steps")
+            val steps: List<Any>,
+            @SerializedName("summary")
+            val summary: String,
+            @SerializedName("weight")
+            val weight: Double
+        ) {
+            data class Admin(
+                @SerializedName("iso_3166_1")
+                val iso31661: String,
+                @SerializedName("iso_3166_1_alpha3")
+                val iso31661Alpha3: String
+            )
+        }
+    }
+
+    data class Waypoint(
+        @SerializedName("distance")
+        val distance: Double,
+        @SerializedName("location")
+        val location: List<Double>,
+        @SerializedName("name")
+        val name: String
+    )
+}
